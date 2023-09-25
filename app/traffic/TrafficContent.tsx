830009b04f951a3e7e1f214fd8e83fe22d3bbfe4
Hong Yong Ching <venushong667@gmail.com>
2023-09-25 22:25:49 +0800

    Add camera image component

---
+++ app/traffic/TrafficContent.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import React, { useState } from "react";
+import LocationTable from "./LocationTable";
+import CameraImage from "./CameraImage";
+
+interface ImageMetadata {
+    height: number,
+    width: number,
+    md5: string
+}
+
+export interface Traffic {
+    id: string,
+    address: string,
+    route: string,
+    neighborhood: string,
+    region: string,
+    image: {
+        url: string,
+        metadata: ImageMetadata
+    },
+    timestamp: string
+}
+
+interface TrafficContentProps {
+    date: Date,
+}
+
+export default function TrafficContent({ date }: TrafficContentProps) {
+    const [selectedLocation, selectLocation] = useState<Traffic>()
+
+    return (
+        <>
+            <LocationTable date={date} selectLocation={selectLocation}/>
+            <CameraImage selectedLocation={selectedLocation}/>
+        </>
+    )
+}
