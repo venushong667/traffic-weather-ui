c7f0d183725172f3c7bfca75f8451da65d4dc44a
Hong Yong Ching <venushong667@gmail.com>
2023-09-26 23:13:53 +0800

    Fix missing forecast type

--- app/weather/utils.tsx
+++ app/weather/utils.tsx
@@ -1,9 +1,26 @@
+enum ForecastType {
+    thundery_showers = 'thundery showers',
+    light_showers = 'light showers',
+    light_rain = 'light rain',
+    moderate_rain = 'moderate rain',
+    partly_cloudy = 'partly cloudy',
+    windy = 'windy',
+    showers = 'showers',
+    fair = 'fair',
+    cloudy = 'cloudy',
+}
+
 export function getWeatherIconPath(forecastType: string, svgType: 'animated' | 'static') {
     const type = forecastType.toLowerCase().trim()
     const time = type.includes('day') ? '/day' : type.includes('night') ? '/night' : ''
-    const svgName = removeDayNight(type).trim()
+    let svgName = removeDayNight(type).trim()
 
-    return `/weather/${svgType}${time}/${svgName}.svg`
+    const foundIcon = Object.values(ForecastType).find(type => svgName.includes(type))
+    if (foundIcon) {
+        svgName = foundIcon
+    }
+    
+    return `/weather/${svgType}${time}/${foundIcon}.svg`
 }
 
 export function removeDayNight(text: string) {
