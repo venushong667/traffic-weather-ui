6254d1cd710747adf15e5e13873017af92619fab
Hong Yong Ching <venushong667@gmail.com>
2023-09-29 01:03:28 +0800

    remove unused files

--- components/circular-loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-interface CircularLoadingProps {
-    className?: string;
-}
-
-export function CircularLoading({ className }: CircularLoadingProps) {
-    return <Loader2 className={ "h-fit w-fit animate-spin dark:invert "+ className } />
-}
