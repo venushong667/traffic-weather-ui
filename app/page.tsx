88d0781d0e1b6634a5b89416ce67dc64d57082c7
Hong Yong Ching <venushong667@gmail.com>
2023-09-29 10:48:23 +0800

    Update now button and shadow dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button'
 import { Traffic } from './traffic/interfaces'
 import TrafficModule from './traffic/TrafficModule'
 import WeatherModule from './weather/WeatherModule'
+import { TimerReset } from 'lucide-react'
 
 
 export default function Home() {
@@ -24,13 +25,16 @@ export default function Home() {
             <div id="traffic" className="h-full flex flex-col mobile:col-span-3 laptop:col-span-2 desktop:col-span-3 gap-7 2xl:overflow-x-auto p-5">
                 <div id="top-bar" className="flex gap-3 flex-row flex-wrap relative translate-x-0 pr-[40px]">
                     <DatePicker date={date} setDate={handleDateChange}></DatePicker>
-                    <Button onClick={() => setDate(new Date())}>Now</Button>
+                    <Button onClick={() => setDate(new Date())}>
+                        <TimerReset className="mr-2 h-5 w-5"/>
+                        Now
+                    </Button>
                     <ModeToggle className="fixed right-0" />
                 </div>
 
                 <TrafficModule date={date} selectedLocation={selectedLocation} selectLocation={selectLocation} />
             </div>
-            <div id="weather" className="shadow mobile:overflow-x-auto mobile:min-h-[470px] laptop:overflow-visible laptop:min-h-[auto] laptop: mobile:col-span-3 laptop:col-span-1">
+            <div id="weather" className="shadow dark:shadow-slate-500 mobile:overflow-x-auto mobile:min-h-[470px] laptop:overflow-visible laptop:min-h-[auto] laptop: mobile:col-span-3 laptop:col-span-1">
                 <WeatherModule date={date} selectedLocation={selectedLocation} />
             </div>
 
